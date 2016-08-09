d6ba03b6c5d12aed0be7c3ed9bebbc48111ead30
Samuel Barrett <sbarrett@cs.utexas.edu>
2016-08-09 13:06:47 -0400

    Adding background

---
+++ background/apodcalvin.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+url=http://apod.nasa.gov/apod/
+#url=http://apod.nasa.gov/apod/ap130213.html
+#url=http://apod.nasa.gov/apod/ap130918.html
+prefix=ch_
+if [ $# -gt 0 ]; then
+  url=$1
+fi
+imageurl=`curl $url 2> /dev/null | grep IMG | cut -d\" -f2` 
+filename=`echo $imageurl | cut -d/ -f3`
+curl http://apod.nasa.gov/apod/$imageurl -o $filename 2> /dev/null
+size=`identify $filename | cut -d' ' -f3`
+
+eog $filename
+echo -n "Use new image? "
+read ans
+
+if [ $ans == "y" -o $ans == "yes" ]; then
+  convert chblank.png -resize $size tmp_resize_chblank.png
+  composite -gravity south tmp_resize_chblank.png $filename calvin.jpg
+  rm tmp_resize_chblank.png
+  feh --bg-scale calvin.jpg
+fi
+rm $filename
+
+#echo "Composite image created: $prefix$filename"
