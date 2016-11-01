ab12113be1cfec8e136a689220a44d39bfb385d6
Samuel Barrett <sam@cogitai.com>
2016-11-01 14:17:24 -0400

    Start keyring on qtile start

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -5,3 +5,8 @@ pnmixer &
 feh --bg-scale /home/sam/dotfiles/background/calvin.jpg
 xinput --set-prop "KMEPC USB Full Speed Gaming Mouse" "Device Accel Constant Deceleration" 2
 xinput --set-prop "DELL0741:00 06CB:7E7E Touchpad" "Synaptics Tap Time" 0
+
+# for keyring
+eval $(gnome-keyring-daemon --start)
+export GNOME_KEYRING_SOCKET
+export GNOME_KEYRING_PID
