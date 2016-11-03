298181c7ae7dfd9438797b356d8414bc1750b1a7
Samuel Barrett <sam@cogitai.com>
2016-11-03 10:39:59 -0400

    Add dropbox to autostart

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+# for keyring
+eval $(gnome-keyring-daemon --start)
+export GNOME_KEYRING_SOCKET
+export GNOME_KEYRING_PID
+
 nm-applet --sm-disable &
 pnmixer &
 feh --bg-scale /home/sam/dotfiles/background/calvin.jpg
 xinput --set-prop "KMEPC USB Full Speed Gaming Mouse" "Device Accel Constant Deceleration" 2
 xinput --set-prop "DELL0741:00 06CB:7E7E Touchpad" "Synaptics Tap Time" 0
 
-# for keyring
-eval $(gnome-keyring-daemon --start)
-export GNOME_KEYRING_SOCKET
-export GNOME_KEYRING_PID
+dropbox start &
