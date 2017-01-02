c74cc7cbb51cfed19f67133d951b2165cafd3127
Samuel Barrett <sam@cogitai.com>
2017-01-02 00:32:34 -0500

    autostart mess with touchpad

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -9,6 +9,11 @@ nm-applet --sm-disable &
 pnmixer &
 feh --bg-scale /home/sam/dotfiles/background/calvin.jpg
 xinput --set-prop "KMEPC USB Full Speed Gaming Mouse" "Device Accel Constant Deceleration" 2
-xinput --set-prop "DELL0741:00 06CB:7E7E Touchpad" "Synaptics Tap Time" 0
+
+TOUCHPAD="DELL0741:00 06CB:7E7E Touchpad"
+xinput --set-prop "$TOUCHPAD" "Synaptics Tap Time" 0
+xinput --set-prop "$TOUCHPAD" "Synaptics Palm Detection" 1
+xinput --set-prop "$TOUCHPAD" "Synaptics Edge Scrolling" 0 0 0
+# xinput --set-prop "$TOUCHPAD" "Synaptics Tap Action" 2 3 0 0 1 3 0
 
 dropbox start &
