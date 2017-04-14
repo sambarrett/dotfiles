e458740eec80849032fdf048446df90787798dcf
Samuel Barrett <sam@cogitai.com>
2017-04-14 18:14:42 -0400

    Compress vids to x264 because it plays with gDrive

--- bin/compress-video.sh
+++ bin/compress-video.sh
@@ -3,4 +3,4 @@
 input_file="$1"
 output_file="$2"
 
-ffmpeg -i "$input_file" -c:v libx265 -preset slow -x265-params crf=22 -c:a libmp3lame -b:a 128k "$output_file"
+ffmpeg -i "$input_file" -c:v libx264 -preset slow -x264-params crf=22 -c:a libmp3lame -b:a 128k "$output_file"
